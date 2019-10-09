2cb680721175cc871ed33108dfd9919ed8470ff5
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-10-09 17:49:57 -0300

    Updates

---
+++ precache-manifest.e9d08058f480af43989586daabbd3232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a8ec598a16147b595a4b3ea688c4ed",
+    "url": "/comment-section/index.html"
+  },
+  {
+    "revision": "433768233ea8281d8439",
+    "url": "/comment-section/static/css/main.54017e93.chunk.css"
+  },
+  {
+    "revision": "b648bad7085e7a53db97",
+    "url": "/comment-section/static/js/2.0848bfc5.chunk.js"
+  },
+  {
+    "revision": "433768233ea8281d8439",
+    "url": "/comment-section/static/js/main.6447cb16.chunk.js"
+  },
+  {
+    "revision": "59740e1e8d72090f9fea",
+    "url": "/comment-section/static/js/runtime-main.0783020e.js"
+  }
+]);
